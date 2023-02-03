0d3caff3fe7b9e646729e7339e2f7907b8cba187
lavish0000 <46993225+lavish0000@users.noreply.github.com>
2023-02-03 12:30:17 +0000

    Update dev to output generated at d087c07

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,6 @@ const tslib_1 = require("tslib");
 const express_1 = (0, tslib_1.__importDefault)(require("express"));
 const router = express_1.default.Router();
 const Item_1 = (0, tslib_1.__importDefault)(require("../modules/Item"));
-router.use('/nft', Item_1.default);
+router.use('/item', Item_1.default);
 exports.default = router;
 //# sourceMappingURL=index.js.map
